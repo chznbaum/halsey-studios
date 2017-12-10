598fa3daff92d0977afb9460d59a9d6671d966ef
Chazona Baum <chznbaum@gmail.com>
2017-12-09 23:33:53 -0500

    implemented sticky header on medium and up

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,7 +1,9 @@
 import HalseySmoothScroll from './modules/HalseySmoothScroll';
 import Modal from './modules/Modal';
 import Tabs from './modules/Tabs';
+import StickyHeader from './modules/StickyHeader';
 
 const halseySmoothScroll = new HalseySmoothScroll();
 const modal = new Modal();
 const tabs = new Tabs();
+const stickyHeader = new StickyHeader();
